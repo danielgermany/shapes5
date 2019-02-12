f7cf221982cfa02439cd973eeb717d489d18917f
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-12 12:53:58 -0500

    Have 4 shapes in seperate corners

--- DGshapes5.py
+++ DGshapes5.py
@@ -11,3 +11,10 @@ gSquare = Polygon(Point(10,590),Point(100,590),Point(100,500),Point(10,500))
 gSquare.setFill(color_rgb(30,230,30))
 gSquare.draw(shapesWin)
 
+rRect = Polygon(Point(490,550),Point(590,550),Point(590,490),Point(490,490))
+rRect.setFill(color_rgb(230,30,30))
+rRect.draw(shapesWin)
+
+pPentagon = Polygon(Point(490,10),Point(470,110),Point(520,150),Point(570,110),Point(550,10))
+pPentagon.setFill(color_rgb(230,230,30))
+pPentagon.draw(shapesWin)
