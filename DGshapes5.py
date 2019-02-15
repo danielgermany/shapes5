51ebf8e88c93f85ccbaf04698bf4eabc9d2a719b
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-15 09:09:53 -0500

    Changed some Polygon functions to Rectangle functions

--- DGshapes5.py
+++ DGshapes5.py
@@ -7,14 +7,15 @@ bTri = Polygon(Point(10, 10), Point(50,100), Point(100,10))
 bTri.setFill(color_rgb(30,30,230))
 bTri.draw(shapesWin)
 
-gSquare = Polygon(Point(10,590),Point(100,590),Point(100,500),Point(10,500))
+gSquare = Rectangle(Point(10,500),Point(100,590))
 gSquare.setFill(color_rgb(30,230,30))
 gSquare.draw(shapesWin)
 
-rRect = Polygon(Point(490,550),Point(590,550),Point(590,490),Point(490,490))
+rRect = Rectangle(Point(490,490),Point(590,550))
 rRect.setFill(color_rgb(230,30,30))
 rRect.draw(shapesWin)
 
-pPentagon = Polygon(Point(490,10),Point(470,110),Point(520,150),Point(570,110),Point(550,10))
-pPentagon.setFill(color_rgb(230,230,30))
-pPentagon.draw(shapesWin)
+yPentagon = Polygon(Point(490,10),Point(470,110),Point(520,150),Point(570,110),Point(550,10))
+yPentagon.setFill(color_rgb(230,230,30))
+yPentagon.draw(shapesWin)
+
