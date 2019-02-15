089d3a05d3348e808eb99e1263c949df792b43dd
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-15 09:28:49 -0500

    Added diamond

--- DGshapes5.py
+++ DGshapes5.py
@@ -1,7 +1,10 @@
 from graphics import *
 
+xWindowSize = 600
+yWindowSize = 600
+
 shapesWin = GraphWin("Shapes 5", 300, 300)
-shapesWin.setCoords(0, 0, 600, 600)
+shapesWin.setCoords(0, 0, xWindowSize, yWindowSize)
 
 bTri = Polygon(Point(10, 10), Point(50,100), Point(100,10))
 bTri.setFill(color_rgb(30,30,230))
@@ -19,3 +22,6 @@ yPentagon = Polygon(Point(490,10),Point(470,110),Point(520,150),Point(570,110),P
 yPentagon.setFill(color_rgb(230,230,30))
 yPentagon.draw(shapesWin)
 
+pDiamond = Polygon(Point(((xWindowSize/2)),((yWindowSize/2)+50)),Point(((xWindowSize/2)+50),((yWindowSize/2))),Point(((xWindowSize/2)),((yWindowSize/2)-50)),Point(((xWindowSize/2)-50),((yWindowSize/2))))
+pDiamond.setFill(color_rgb(230,30,230))
+pDiamond.draw(shapesWin)
